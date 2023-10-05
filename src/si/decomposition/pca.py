b8dbc1ccbfdc96dd43b297181192ad379e2bb9fe
Seixal97 <103841111+paulo-seixal@users.noreply.github.com>
2023-10-05 14:59:51 +0100

    [UPDATE] pca.py now adresses the case where n_components is a float

--- src/si/decomposition/pca.py
+++ src/si/decomposition/pca.py
@@ -1,5 +1,6 @@
 import numpy as np
 from si.data.dataset import Dataset
+from typing import Union
 
 class PCA:
     '''
@@ -20,11 +21,16 @@ class PCA:
     explained_variance: np.ndarray
         Explained variance.
     '''
-    def __init__(self, n_components: int):
+    def __init__(self, n_components: Union[int, float]):
+        if not isinstance(n_components, (int, float)):
+            raise ValueError("Invalid value for n_components. It must be an integer or a float between 0 and 1.0.")
+        
         self.n_components = n_components
         self.components = None
         self.mean = None
         self.explained_variance = None
+        self.explained_variance_ratio = None
+
 
     def _get_centered_data(self, dataset: Dataset) -> np.ndarray:
         '''
@@ -56,7 +62,7 @@ class PCA:
         self.U, self.S, self.V = np.linalg.svd(self._get_centered_data(dataset), full_matrices=False)
 
         #extracting the components (first n_components columns of V)
-        self.components = self.V.T[:, :self.n_components]
+        self.components = self.V[:self.n_components]
 
         return self.components
     
@@ -72,7 +78,9 @@ class PCA:
 
         #extracting the explained variance
         ev = (self.S ** 2) / (self._get_centered_data(dataset).shape[0] - 1)
-        self.explained_variance = ev[:, :self.n_components]
+        self.explained_variance = ev[:self.n_components]
+        self.explained_variance_ratio = self.explained_variance / np.sum(self.explained_variance)
+
         return self.explained_variance
     
     def fit(self, dataset: Dataset) -> 'PCA':
@@ -89,6 +97,16 @@ class PCA:
         PCA
             PCA object.
         '''
+        #adresses the case where n_components is a float between 0 and 1.0
+        if 0 < self.n_components < 1.0:
+            # calculate the explained variance ratio for a large number of components
+            full_pca = PCA(n_components=dataset.X.shape[1])
+            full_pca.fit(dataset)
+            ratio_cumsum = np.cumsum(full_pca.explained_variance_ratio)
+            
+            # shorten the number of components to the number that explains the given variance
+            self.n_components = np.searchsorted(ratio_cumsum, self.n_components) + 1
+        
         self._get_components(dataset)
         self._get_explained_variance(dataset)
         return self
@@ -127,3 +145,18 @@ class PCA:
         '''
         self.fit(dataset)
         return self.transform(dataset)
+    
+if __name__ == '__main__':
+    from si.io.csv_file import read_csv
+    iris = read_csv('datasets/iris/iris.csv',sep=',',features=True, label=True)
+
+    pca = PCA(n_components=0.9)
+    pca.fit_transform(iris)
+    print(pca.n_components)
+
+    from sklearn.decomposition import PCA as PCA_sklearn
+    pca_sklearn = PCA_sklearn(n_components=0.9)
+    pca_sklearn.fit_transform(iris.X)
+    print(pca_sklearn.n_components_)
+
+
